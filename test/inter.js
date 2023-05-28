d65f43b6135ccefd5c386187aaeb3e98b16f0738
Anders Rundgren <anders.rundgren.net@gmail.com>
2023-05-28 19:22:14 +0200

    added test files to github

---
+++ test/inter.js
@@ -0,0 +1,29 @@
+﻿const CBOR = require('../src/cbor-js-api.js');
+const prompt = require('prompt-sync')({sigint: true});
+
+'use strict';
+
+while (true) {
+  let text = prompt('Input BigInt (n or 0xn) : ').replace(/\ /g, '');
+  let radix = 10;
+  if (text.startsWith('0x')) {
+    test = text.substring(2);
+    radix = 16;
+  }
+  let bigInt = BigInt(text, radix);
+  let encodedCbor = CBOR.BigInt(bigInt).encode();
+  console.log("Encoded: " + CBOR.toHex(encodedCbor));
+  let decodedCbor =  CBOR.decode(encodedCbor);
+  console.log("Decoded: " + 
+              (radix == 10 ? '' : '0x') +
+              decodedCbor.getBigInt().toString(radix) + 
+              " type=CBOR." +  
+              decodedCbor.constructor.name);
+  try {
+    console.log("getInt(): " + 
+                (radix == 10 ? '' : '0x') +
+                decodedCbor.getInt().toString(radix));
+  } catch (error) {
+    console.log(error.toString());
+  }
+}
